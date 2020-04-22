6bd9f8911a6dd93ce47f82abc66dc2ecab211030
Arthur Gunn <arthur@gunn.co.nz>
2020-04-22 22:12:11 +1200

    Allow usePureStore to take a getter and work on a subset of the state.

--- src/react.test.tsx
+++ src/react.test.tsx
@@ -6,9 +6,9 @@ import createStore from "./react"
 
 
 
-const store = createStore({ count: 0 })
+const counterStore = createStore({ count: 0 })
 const Counter = ()=> {
-  const [state, update] = store.usePureStore()
+  const [state, update] = counterStore.usePureStore()
 
   return (
     <div>
@@ -41,7 +41,7 @@ describe("Using a component with usePureStore()", ()=> {
     expect(count).toBe("0")
   })
 
-  test("Works with interaction and updates", async ()=> {
+  test("Works with interaction and updates", ()=> {
     act(()=> { ReactDOM.render(<Counter />, container) })
 
     const [inc, dec] = container.querySelectorAll("button") as unknown as HTMLButtonElement[]
@@ -56,5 +56,136 @@ describe("Using a component with usePureStore()", ()=> {
 
     const count = container.querySelector("span").textContent
     expect(count).toBe("11")
+  })
+})
+
+
+
+const store = createStore({
+  lists: [
+    {
+      date: new Date(),
+      completed: false,
+      items: [
+        {
+          name: "eggs",
+          purchased: false
+        },
+        {
+          name: "bread",
+          purchased: true
+        },
+        {
+          name: "eggs",
+          purchased: false
+        }
+      ]
+    },
+    {
+      date: new Date(+new Date() - 86400000),
+      completed: true,
+      items: [
+        {
+          name: "vinegar",
+          purchased: true
+        }
+      ]
+    }
+  ]
+})
+const ShoppingApp = ()=> {
+  const [state] = store.usePureStore()
+
+  return (
+    <div className="shopping-app">
+      <h1>
+        Shopping
+        <small>
+          { state.lists.length } { "lists, " }
+          { state.lists.filter(d=> !d.completed).length } uncompleted
+        </small>
+      </h1>
+
+      <NextDueList />
+    </div>
+  )
+}
+
+const NextDueList = React.memo(()=> {
+  const [state, update] = store.usePureStore(s=> s.lists.find(l=> l.completed==false))
+
+  // If there are no lists due:
+  if (!state) return null
+
+  const {date, completed, items} = state
+
+  return (
+    <div className="list">
+      <h3>
+        List for {date.toLocaleString()}
+        <small>
+          { items.length } items, { items.filter(d=> !d.purchased).length } unpurchased
+        </small>
+      </h3>
+
+      <input
+        type="checkbox"
+        checked={completed}
+        onChange={()=> update({ completed: !completed })}
+      />
+
+      <ul>
+        {
+          items.map((item, i)=>
+            <li key={i}>
+              <label>
+                { item.name }
+
+                <input
+                  type="checkbox"
+                  checked={completed}
+                  onChange={e=> update(s=> s.items[i].purchased = e.target.checked)}
+                />
+              </label>
+            </li>
+          )
+        }
+      </ul>
+    </div>
+  )
+})
+
+
+describe("Using usePureStore with a getter", ()=> {
+  test("Renders as expected", ()=> {
+    act(()=> { ReactDOM.render(<ShoppingApp />, container) })
+
+    const text = container.querySelector("h1 small").textContent
+    expect(text).toBe("2 lists, 1 uncompleted")
+
+    const listDescription = container.querySelector("h3 small").textContent
+    expect(listDescription).toBe("3 items, 2 unpurchased")
+  })
+
+  test("Works with interaction and updates", async ()=> {
+    act(()=> { ReactDOM.render(<ShoppingApp />, container) })
+
+    act(()=> {
+      const eggCheckbox = container.querySelector(".list ul input")
+
+      eggCheckbox.dispatchEvent(new MouseEvent('click', { bubbles: true }))
+    })
+
+    const listDescription = container.querySelector("h3 small").textContent
+    expect(listDescription).toBe("3 items, 1 unpurchased")
+
+
+    act(()=> {
+      const checkbox = container.querySelector(".list > input")
+      checkbox.dispatchEvent(new MouseEvent('click', { bubbles: true }))
+    })
+
+    const appDescription = container.querySelector("h1 small").textContent
+    expect(appDescription).toBe("2 lists, 0 uncompleted")
   })
 })
--- src/react.ts
+++ src/react.ts
@@ -7,9 +7,15 @@ function useForceUpdate() {
 }
 
 class PureStoreReact<S, T> extends PureStore<S, T> {
-  usePureStore = ()=> {
+  usePureStore                      (): readonly [T, (updater: Partial<T> | ((e: T) => void)) => void]
+  usePureStore <X>(getter?: (s: T)=>X): readonly [X, (updater: Partial<X> | ((e: X) => void)) => void]
+  usePureStore(getter?) {
+    if (getter) {
+      return new PureStoreReact(this, getter).usePureStore()
+    }
+
     const forceUpdate = useForceUpdate()
-    React.useEffect(()=> this.subscribe(forceUpdate), [])
+    React.useEffect(()=> this.root.subscribe(forceUpdate), [])
 
     return [this.state, this.update] as const
   }
